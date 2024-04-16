c7a4adc46e29f50b7615f9cba8e66eb1887b6452
MKavian89 <158145150+MKavian89@users.noreply.github.com>
2024-04-16 23:43:51 +0330

    C++ Carwash
    توضیحاتو عرض کردم

---
+++ C++ Car Wash.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <cstring>
+#include <Windows.h>
+using namespace std;
+int main(){
+	int money;
+	cout << "Welcome To The C++ Car Wash!"<<endl;
+	cout << "What is your money for this carwash to rank your servise ( Of 10 Starting to 100 ) :"<<endl;
+	cin>>money;
+	switch  (money){
+		case 10 :
+			case 11:
+				case 12:
+					case 13:
+						case 14:
+							case 15:
+								case 16:
+									case 17:
+										case 18:
+											case 19:
+												case 20:
+												cout <<"Basic Is Good For You"<<endl;
+												cout << "The Basic Was Have: \n The Wash Car \n Puffing car \n and Basic Drying"<<endl;
+												break;
+													case 21:
+														case 22:
+															case 23:
+																case 24:
+																	case 25:
+																		case 26:
+																			case 27 :
+																				case 28:
+																					case 29:
+													case 30:
+														case 31:
+															case 32:
+																case 33:
+																	case 34:
+																		case 35:
+																			case 36:
+																				case 37:
+																					case 38 :
+																						case 39:
+														case  40:
+															case 41:
+																case 42:
+																	case 43:
+																		case 44:
+																			case 45:
+																				case 46:
+																					case 47:
+																						case 48:
+																							case 49:
+																								
+															case 50:
+															cout << "Good! This Is The Beeter rank car service"<<endl;
+															cout << "The Better Was Have: \n The super Wash Car \n Good Puffing car \n  Better Drying \n and The Cleaning inside the car"<<endl;
+															break;
+															case 51:
+																case 52:
+																	case 53:
+																		case 54:
+																			case 55:
+																				case 56:
+																					case 57:
+																						case 58:
+																							case 59:
+															case 60:
+																case 61:
+																	case 62:
+																		case 63:
+																			case 64:
+																				case 65:
+																					case 66:
+																						case 67:
+																							case 68:
+																								case 69:
+																case 70:
+																	case 71:
+																		case 72:
+																			case 73:
+																				case 74:
+																					case 75:
+																						case 76:
+																							case 77:
+																								case 78:
+																									case 79:
+																	case 80:
+																	cout << "WoW! You Have a Ultra rank!"<<endl;
+																	cout << "The Ultra Was Have: \n The super Wash Car \n Good Puffing car \n  Ultra Drying \n The Cleaning inside the car \n "<<endl;
+																	break;
+																	case 81:
+																		case 82:
+																			case 83:
+																				case 84:
+																					case 85:
+																						case 86:
+																							case 87:
+																								case 88:
+																									case 89:
+																										case 90:
+																											case 91:
+																												case 92:
+																													case 93:
+																														case 94:
+																															case 95:
+																																case 96:
+																																	case 97:
+																																		case 98:
+																																			case 99:
+																	case 100:
+																		cout << "Great! The VIP Rank!"<<endl;
+																		cout << "The Best And Full Rank it Is!"<<endl;
+																		break;
+																		default :
+																			cout << "This Fill Is Incorect!"<<endl;
+																			return 0;
+	}
+}
